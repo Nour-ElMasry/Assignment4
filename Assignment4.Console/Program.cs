89e24a374ad47e8eeffda6b4347a0faaa177c09c
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-07-25 19:36:59 +0300

    more functoinalities

--- Assignment4.Console/Program.cs
+++ Assignment4.Console/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assignment4.Domain;
+
+var jerry = new ChatBot("Jerry", "I was created by Nour Eldin");
+var flag = true;
+
+jerry.instructions();
+Console.WriteLine("\n///////////////////////////////////////////\n");
+jerry.Intro();
+
+while (flag)
+{
+    var input = jerry.chat();
+    Console.WriteLine();
+    switch (input)
+    {
+        case "1":
+            jerry.EnterUsername();
+            break;
+        case "2":
+            jerry.EnterBirthDate();
+            break;
+        case "3":
+            jerry.GetDate();
+            break;
+        case "4":
+            jerry.GetTime();
+            break;
+        case "5":
+            jerry.UserInfo();
+            break;
+        case "6":
+            flag = false;
+            break;
+        default:
+            jerry.Msg("Not a command! please see list of instructions!");
+            break;
+    }
+}
--- Assignment4.Domain/ChatBot.cs
+++ Assignment4.Domain/ChatBot.cs
@@ -4,28 +4,76 @@ using System.Text.RegularExpressions;
 
 namespace Assignment4.Domain
 {
+    public class User
+    {
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public User(string name)
+        {
+           Name = name;
+        }
+
+        public override string ToString()
+        {
+            if(BirthDate == null)
+                return $"\n       Username: {Name}\n      BirthDate: Not Provided";
+            return $"       Username: {Name}\n      BirthDate: {DateOnly.FromDateTime((DateTime)BirthDate):dd/MM/yyyy}";
+        }
+    }
     public class ChatBot
     {
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public User User { get; set; }
+
         string _syntaxError = "Please follow given syntax";
+
+        public ChatBot(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            User = new User("user");
+        }
+
+        public void instructions() {
+            Console.WriteLine("List of commands: ");
+            Console.WriteLine(" 1 - Enter your name\n 2 - Enter date of birth\n 3 - Get current date\n 4 - Get current time\n 5 - User info\n 6 - exit");
+        }
         public void Intro() 
         {
             Console.WriteLine($"Hello! My name is {Name}.\n{Description}");
         }
 
-        public int Greeting(string username) 
+        public void Msg(string msg) 
         {
+            Console.WriteLine($"{ Name }: { msg }");
+        }
+
+        public string chat() {
+            Console.Write($"{ User.Name }: ");
+            return Console.ReadKey().KeyChar.ToString();
+        }
+
+        public int EnterUsername() 
+        {
+            if (User.Name != "user") {
+                Msg("Name already introduced!");
+                return -1;
+            }
+
+            Msg("Enter your full name:");
+            var username = Console.ReadLine();
+
             if (string.IsNullOrEmpty(username)) 
             {
-                Console.WriteLine("String is Empty!!");
+                Msg("String is Empty!!");
                 return -1;
             }
 
             var fullName = username.Split(" ");
             if (fullName.Length > 2) {
-                Console.WriteLine(_syntaxError);
+                Msg(_syntaxError);
                 return -1;
             }
 
@@ -35,13 +83,64 @@ namespace Assignment4.Domain
             {
                 if (!charCheck.IsMatch(fullName[i]))
                 {
-                    Console.WriteLine(_syntaxError);
+                    Msg(_syntaxError);
                     return -1;
                 }
             }
 
-            Console.WriteLine($"Greetings {fullName[1]}! Happy to meet you!");
+            User.Name = username;
+            Msg($"Greetings {fullName[0]}! Happy to meet you!");
             return 0;
+        }
+
+        private bool IsDateTime(string d)
+        {
+            DateTime tempDate;
+            if (DateTime.TryParse(d, out tempDate))
+            {
+                User.BirthDate = tempDate;
+                return true;
+            }
+            return false;
+        }
+
+        public int EnterBirthDate() {
+            if (User.BirthDate == null)
+            {
+                Msg("Birth date already introduced!");
+                return -1;
+            }
+
+            Msg("Enter your birth date: (Syntax: yyyy/mm/dd)");
+            var bd = Console.ReadLine();
+
+            if (bd == null)
+                return -1;
+            
+            if (IsDateTime(bd)) 
+            {
+                var userBd = User.BirthDate;
+                if (DateTime.Now.Year - userBd.Value.Year < 0)
+                    return -1;
+                                
+                Console.WriteLine($"You are {DateTime.Now.Year - userBd.Value.Year} years old!");
+            }
+            return 0;
+        }
+
+        public void GetDate() 
+        {
+            Msg($"Today's date is: {DateTime.Now:dd/MM/yyyy}");
+        }
+
+        public void GetTime()
+        {
+            Msg($"Current time is: {TimeOnly.FromDateTime(DateTime.Now)}");
+        }
+
+        public void UserInfo()
+        {
+            Msg(User.ToString());
         }
     }
 }
