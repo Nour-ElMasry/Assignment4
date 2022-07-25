259ee8cb3203cacef134dfb4b9dcf65d07bf42ce
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-07-25 16:54:05 +0300

    first commit

---
+++ Assignment4.Domain/ChatBot.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Text.RegularExpressions;
+
+namespace Assignment4.Domain
+{
+    public class ChatBot
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        string _syntaxError = "Please follow given syntax";
+        public void Intro() 
+        {
+            Console.WriteLine($"Hello! My name is {Name}.\n{Description}");
+        }
+
+        public int Greeting(string username) 
+        {
+            if (string.IsNullOrEmpty(username)) 
+            {
+                Console.WriteLine("String is Empty!!");
+                return -1;
+            }
+
+            var fullName = username.Split(" ");
+            if (fullName.Length > 2) {
+                Console.WriteLine(_syntaxError);
+                return -1;
+            }
+
+            var charCheck = new Regex(@"^[A-Za-z]+$");
+
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (!charCheck.IsMatch(fullName[i]))
+                {
+                    Console.WriteLine(_syntaxError);
+                    return -1;
+                }
+            }
+
+            Console.WriteLine($"Greetings {fullName[1]}! Happy to meet you!");
+            return 0;
+        }
+    }
+}
